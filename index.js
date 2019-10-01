11a62aed937285996999370c44d1a464bf3b0e23
Nataniel López <nataniel.lopez@fizzmod.com>
2019-10-01 18:02:39 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { EventEmitter } = require('./lib');
+
+module.exports = EventEmitter;
---
+++ tests/event-emitter-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+
+const sandbox = require('sinon').createSandbox();
+
+const EventEmitter = require('./../index');
+
+const EventEmitterError = require('./../lib/event-emitter-error')
+
+describe('EventEmitter', () => {
+
+	// your tests here...
+
+});
