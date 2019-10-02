0d5f97250efdced8c66e90a5240ab3ac9e63566d
Nataniel López <nataniel.lopez@fizzmod.com>
2019-10-02 13:07:04 -0300

    First functional release

--- README.md
+++ README.md
@@ -3,20 +3,63 @@
 [![Build Status](https://travis-ci.org/janis-commerce/event-emitter.svg?branch=master)](https://travis-ci.org/janis-commerce/event-emitter)
 [![Coverage Status](https://coveralls.io/repos/github/janis-commerce/event-emitter/badge.svg?branch=master)](https://coveralls.io/github/janis-commerce/event-emitter?branch=master)
 
-A package for emit events
+A package for emit events to janis-events
 
 ## Installation
 ```sh
 npm install @janiscommerce/event-emitter
 ```
 
+## Configuration
+### ENV variables
+`JANIS_SERVICE_NAME` (required): The name of the service that will emit the event.
+
+### Important
+This package uses [@janiscommerce/microservice-call](https://www.npmjs.com/package/@janiscommerce/microservice-call), it's strongly recommended read this package docs before continue.
+
 ## API
 
+### **`emit(event)`**
+
+Emits an event to janis-events
+
+#### Event parameter
+
+The event parameter is an `[Object]` and have the next struct:
+- **`entity [String]`** (required): The name of the entity that is emiting the event
+- **`event [String]`** (required): The event name
+- **`client [String]`** (optional): The client code name
+- **`id [Number|String]`** (optional): The ID of the entity that is emiting the event
+
+## Errors
+
+The errors are informed with a `EventEmitterError`.  
+This object has a code that can be useful for a correct error handling.  
+The codes are the following:  
+
+| Code | Description                    |
+|------|--------------------------------|
+| 1    | Invalid event                  |
+| 2    | Invalid event properties       |
+| 3    | Unknown/empty service name     |
+| 4    | MicroserviceCall Error         |
 
 ## Usage
 ```js
 const EventEmitter = require('@janiscommerce/event-emitter');
 
-```
+(async () => {
 
-## Examples
+	try {
+		await EventEmitter.emit({
+			entity: 'some-entity',
+			event: 'some-event',
+			client: 'some-client',
+			id: 1
+		})
+	} catch(err) {
+		throw err;
+	}
+
+})();
+```
--- tests/event-emitter-test.js
+++ tests/event-emitter-test.js
@@ -1,15 +1,132 @@
 'use strict';
 
 const assert = require('assert');
-
 const sandbox = require('sinon').createSandbox();
+const MicroserviceCall = require('@janiscommerce/microservice-call');
 
 const EventEmitter = require('./../index');
-
-const EventEmitterError = require('./../lib/event-emitter-error')
+const EventEmitterError = require('./../lib/event-emitter-error');
 
 describe('EventEmitter', () => {
 
-	// your tests here...
+	beforeEach(() => {
+		sandbox.restore();
+	});
+
+	describe('_validateEvent()', () => {
+
+		it('should not throw when the received event is correct', () => {
+
+			const event = {
+				entity: 'some-entity',
+				event: 'some-event',
+				client: 'some-client',
+				id: 1
+			};
+
+			assert.doesNotThrow(() => EventEmitter._validateEvent(event));
+		});
+
+		it('should throw when the received event is not an object', () => {
+			assert.throws(() => EventEmitter._validateEvent('string'), {
+				name: 'EventEmitterError',
+				code: EventEmitterError.codes.INVALID_EVENT
+			});
+		});
+
+		it('should throw when the received event is an array', () => {
+			assert.throws(() => EventEmitter._validateEvent(['array']), {
+				name: 'EventEmitterError',
+				code: EventEmitterError.codes.INVALID_EVENT
+			});
+		});
+
+		context('when the received event doesn\'t have the required fields', () => {
+
+			it('should throw if event.entity not exists', () => {
+				assert.throws(() => EventEmitter._validateEvent({ event: 'something' }), {
+					name: 'EventEmitterError',
+					code: EventEmitterError.codes.INVALID_EVENT_PROPERTIES
+				});
+			});
+
+			it('should throw if event.event not exists', () => {
+				assert.throws(() => EventEmitter._validateEvent({ entity: 'something' }), {
+					name: 'EventEmitterError',
+					code: EventEmitterError.codes.INVALID_EVENT_PROPERTIES
+				});
+			});
+		});
+
+		context('when there are bad types in the event properties', () => {
+
+			const event = {
+				event: 'some-event',
+				entity: 'some-entity'
+			};
+
+			it('should throw if event.client exists but is not a string', () => {
+				assert.throws(() => EventEmitter._validateEvent({ ...event, client: 1 }), {
+					name: 'EventEmitterError',
+					code: EventEmitterError.codes.INVALID_EVENT_PROPERTIES
+				});
+			});
+
+			it('should throw if event.id exists but is not a number or string', () => {
+				assert.throws(() => EventEmitter._validateEvent({ ...event, id: {} }), {
+					name: 'EventEmitterError',
+					code: EventEmitterError.codes.INVALID_EVENT_PROPERTIES
+				});
+			});
+		});
+	});
+
+	describe('emit()', () => {
+
+		it('should return true when MicroserviceCall.post not rejects', async () => {
+
+			const event = {
+				entity: 'some-entity',
+				event: 'some-event'
+			};
+
+			const msCallMock = sandbox.mock(MicroserviceCall.prototype).expects('post')
+				.withExactArgs('events', 'event', 'emit', { ...event, service: 'some-service' })
+				.returns();
+
+			assert.deepStrictEqual(await EventEmitter.emit(event), true);
+
+			msCallMock.verify();
+		});
+
+		it('should throw when MicroserviceCall.post rejects', async () => {
+
+			const event = {
+				entity: 'some-entity',
+				event: 'some-event'
+			};
+
+			const msCallMock = sandbox.mock(MicroserviceCall.prototype).expects('post')
+				.withExactArgs('events', 'event', 'emit', { ...event, service: 'some-service' })
+				.rejects();
+
+			await assert.rejects(EventEmitter.emit(event), {
+				name: 'EventEmitterError',
+				code: EventEmitterError.codes.MS_CALL_ERROR
+			});
+
+			msCallMock.verify();
+		});
+
+		it('should throw when service name env not exists', async () => {
+
+			sandbox.stub(EventEmitter, '_serviceName').get(() => undefined);
+
+			await assert.rejects(EventEmitter.emit({}), {
+				name: 'EventEmitterError',
+				code: EventEmitterError.codes.NO_SERVICE_NAME
+			});
+		});
+	});
 
 });
